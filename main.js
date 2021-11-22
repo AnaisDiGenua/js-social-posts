875ed27bf93d4bb26aa90bb4bdaa4dab833da770
AnaisDiGenua <anais.digenua5@gmail.com>
2021-11-22 16:38:59 +0100

    first step

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+// - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine (non tutti i post devono avere una immagine), numero di likes.
+// Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>) 
+// - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+//  - Rendiamo il tasto "Mi Piace" cliccabile con incremento del counter dei like
+// - Formattare le date in formato italiano (gg/mm/aaaa)
+// - Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+// - Al click su un pulsante "Mi Piace" di un post, incrementare il contatore di like al post e cambiare colore al testo del bottone.
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +65,66 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+
+
+
+// richiamo la funzione per stampare i post
+printPost();
+
+
+
+
+// funzione per stampare i post
+function printPost() {
+    for(let i = 0; i < posts.length; i++) {
+
+        // richiamo la funzione
+        generatePost(posts[i]);
+    }
+}
+
+
+
+
+// funzione per generare un post
+function generatePost(array) {
+
+    // variabile contenitore post
+    const container = document.querySelector('.posts-list');
+    container.innerHTML += 
+    `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${array['author']['image']} alt=${array['author']['name']}>                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${array['author']['name']}</div>
+                        <div class="post-meta__time">${array['created']}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${array['content']}</div>
+            <div class="post__image">
+                <img src=${array['media']} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${array['likes']}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+}
